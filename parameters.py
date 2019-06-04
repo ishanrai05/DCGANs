1e6c98a935c9d02d85a46ea2ff12e4ab666608d6
ishanrai05 <ishanrai05@gmail.com>
2019-06-04 19:46:41 +0530

    Adds parametes

---
+++ parameters.py
@@ -0,0 +1,21 @@
+# Size of z latent vector
+num_z = 100
+
+# Size of feature maps in generator
+num_feat_gen = 64
+
+# Size of feature maps in disciminator
+num_feat_disc = 64
+
+# number of channels in input image
+num_channels = 3
+
+image_size = 64
+
+batch_size = 128
+
+# learning rate
+lr = 0.0002
+
+# Beta1 hyperparam for Adam optimizers
+beta1 = 0.5
